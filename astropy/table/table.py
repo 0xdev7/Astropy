0xdev7/Astropy

commit e544b547053925faba30832ea14ad02590f12a7c

Update logic to detect scalar input needing broadcasting

astropy/table/table.py
@@ -1762,8 +1762,7 @@ class Table:
 
         # Make col data shape correct for scalars.  The second test is to allow
         # broadcasting an N-d element to a column, e.g. t['new'] = [[1, 2]].
-        if ((len(col.shape) == 0 or (len(col.shape) > 1 and col.shape[0] == 1))
-                and len(self) > 0):
+        if (col.shape == () or col.shape[0] == 1) and len(self) > 0:
             new_shape = (len(self),) + getattr(col, 'shape', ())[1:]
             if isinstance(col, np.ndarray):
                 col = np.broadcast_to(col, shape=new_shape,

astropy/table/tests/test_table.py
@@ -2337,13 +2337,11 @@ def test_tolist():
 
 
 def test_broadcasting_8933():
-    """Small API change re: broadcasting in #8933"""
+    """Explicitly check re-work of code related to broadcasting in #8933"""
     t = table.Table([[1, 2]])  # Length=2 table
     t['a'] = [[3, 4]]  # Can broadcast if ndim > 1 and shape[0] == 1
     t['b'] = 5
+    t['c'] = [1]  # Treat as broadcastable scalar, not length=1 array (which would fail)
     assert np.all(t['a'] == [[3, 4], [3, 4]])
     assert np.all(t['b'] == [5, 5])
-
-    with pytest.raises(ValueError) as exc:
-        t['c'] = [1]  # This broadcasted to [1, 1] before #8933, but should not have.
-    assert 'Inconsistent data column lengths' in str(exc)
+    assert np.all(t['c'] == [1, 1])

commit 767226fe402fd560730ad00a1fbf0b6711ef6352

Fix and test a regression introduced by e544b547

astropy/table/table.py
@@ -1771,6 +1771,10 @@ class Table:
                 col = col._apply(np.broadcast_to, shape=new_shape,
                                    subok=True)
 
+            # broadcast_to() results in a read-only array.  Apparently it only changes
+            # the view to look like the broadcasted array.  So copy.
+            col = col_copy(col)
+
         name = col.info.name
 
         # Ensure that new column is the right length

astropy/table/tests/test_table.py
@@ -2345,3 +2345,7 @@ def test_broadcasting_8933():
     assert np.all(t['a'] == [[3, 4], [3, 4]])
     assert np.all(t['b'] == [5, 5])
     assert np.all(t['c'] == [1, 1])
+
+    # Test that broadcasted column is writeable
+    t['c'][1] = 10
+    assert np.all(t['c'] == [1, 10])